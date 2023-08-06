4e8882a4f78b41ea38e80a5b0c1746187a5251c4
paytonncourt96 <140280443+paytonncourt96@users.noreply.github.com>
2023-08-06 14:33:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.graph_objects as go
 
 def Bio():
     bio_header = st.container()
@@ -8,6 +9,22 @@ def Skills():
     skill_header = st.container()
     with skill_header:
         st.title("Skills")
+    skills = ["Data Visualization", "Machine Learning", "Data Analysis", "Data Cleaning", "Python", "Java", "R", "SQL/noSQl", "Pandas", "NumPy", "MatPlotLib", "seaborn", "Tensorflow", "Keras", "Torch", "Statistical Analysis"]
+    skill_levels = [80, 90, 70, 85, 70, 90, 65, 70, 90, 90, 90, 90, 90, 90, 90, 90]
+        fig = go.Figure(data=go.Bar(x=skills, y=skill_levels, marker_color="rgb(65, 168, 121)"))
+
+    # Update chart layout for better visualization
+    fig.update_layout(
+        title="My Skills",
+        xaxis_title="Skill",
+        yaxis_title="Skill Level",
+        xaxis=dict(tickfont_size=14),
+        yaxis=dict(tickfont_size=14),
+        margin=dict(l=50, r=50, t=50, b=50),
+    )
+
+    # Display the chart using Streamlit
+    st.plotly_chart(fig)
 def Projects():
     project_header = st.container()
     with project_header:
