8d55ab5e2b90159ed227dcfd862e0c4bb4cd4c7e
paytonncourt96 <140280443+paytonncourt96@users.noreply.github.com>
2023-08-06 15:14:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def Bio():
         st.title("Welcome to my bio!")
     with st.container():
         st.header("Biography")
-        st.write("Bio: Hello! I am currently working for Indiana University School of Medicine as a Financial Analyst for clinical trials while pursuing a part-time degree in Data Science at Indiana University Bloomington. I am eager and well-prepared to embrace Data Science opportunities, having gained invaluable knowledge and experience through both my previous job roles and educational pursuits.") 
+        st.write("Bio: Hello! I am currently working for Indiana University School of Medicine as a Financial Analyst for clinical trials while pursuing a part-time Master's degree in Data Science at Indiana University Bloomington. I am eager and well-prepared to embrace Data Science opportunities, having gained invaluable knowledge and experience through both my previous job roles and educational pursuits.") 
         st.write("My main interest/emphasis within my education has been Machine Learning. One of the most exciting projects I have had the opporunity to work on is an image detection program utilizing many well-known architectures of Convolutional neural networks.")
         st.write("Age: 26")
         st.write("Location: Indianapolis, Indiana, United States")
         st.write("Occupation: Financial Analyst")
+        st.write("Education: Bachelor's of Science in Mathematics(Graduated 2019) and Bachelor's of Arts in Physics(Graduated 2019)")
         st.write("Interests: Hiking, Biking, Reading(mainly science fiction!), and playing fetch with my dog Bjorn ")
         st.write("Contact: paytonncourt96@gmail.com")
         st.write("Connect with me!")
