cc8e35f85dafe76757fd113c5d51bde69669f133
paytonncourt96 <140280443+paytonncourt96@users.noreply.github.com>
2023-08-07 10:42:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -28,16 +28,16 @@ def Skills():
    
     st.table(skills_df)
     
-def Projects():
-    project_header = st.container()
-    with project_header:
-        st.title("Current Projects")
+#def Projects():
+   # project_header = st.container()
+    #with project_header:
+        #st.title("Current Projects")
 
 def main():
     header = st.container()
     with header:
         st.title("Courtney Shammas")
-    buttons = st.sidebar.radio("Select content",["Bio", "Skills", "Projects"])
+    buttons = st.sidebar.radio("Select content",["Bio", "Skills"])#"Projects"
     image_url = "https://github.com/paytonncourt96/OnlineCV/raw/main/CV_picture1.jpg"
     st.image(image_url,  width=200, use_column_width=False)
        
@@ -46,8 +46,8 @@ def main():
         Bio()
     elif buttons == "Skills":
         Skills()
-    elif buttons == "Projects":
-        Projects()
+    #elif buttons == "Projects":
+        #Projects()
     else:
         st.warning("Please select an option: ")
         
