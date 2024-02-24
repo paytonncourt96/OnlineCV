679fd3f90fe33606c36015a69ece4cbec8b4b76c
paytonncourt96 <140280443+paytonncourt96@users.noreply.github.com>
2024-02-24 08:54:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import pandas as pd
 def Bio():
     with st.container():
         st.header("Biography")
-        st.write("Hello! I am currently working for Indiana University School of Medicine as a Financial Analyst for clinical trials while pursuing a part-time Master's degree in Data Science at Indiana University Bloomington. I am eager and well-prepared to embrace Data Science opportunities, having gained invaluable knowledge and experience through both my previous job roles and educational pursuits.") 
+        st.write("Hello! I am currently working for the state of Indiana as a Data Scientist at the Family and Social Services Administration(FSSA) while pursuing a part-time Master's degree in Data Science at Indiana University Bloomington. I am eager and well-prepared to embrace Data Science opportunities, having gained invaluable knowledge and experience through both my previous job roles and educational pursuits.") 
         st.write("My main interest/emphasis within my education has been Machine Learning. One of the most exciting projects I have had the opportunity to work on is an image detection program utilizing many well-known architectures of Convolutional neural networks.")
         st.write("Age: 27")
         st.write("Location: Indianapolis, Indiana, United States")
-        st.write("Occupation: Financial Analyst")
+        st.write("Occupation: Data Scientist")
         st.write("Education: Bachelor's of Science in Mathematics(Graduated 2019) and Bachelor's of Arts in Physics(Graduated 2019)")
         st.write("Interests: Hiking, Biking, Reading(mainly science fiction!), and playing fetch with my dog Bjorn ")
         st.write("Contact: paytonncourt96@gmail.com")
@@ -20,8 +20,8 @@ def Skills():
     with skill_header:
         st.title("Skills")
     skills_data = { 
-        "Skill": ["Data Visualization", "Machine Learning", "Data Analysis", "Data Cleaning", "Python", "Java", "R", "SQL/noSQL", "Pandas", "NumPy", "MatPlotLib", "seaborn", "Tensorflow", "Keras", "Torch", "Statistical Analysis"],
-        "Years of Experience":  [4, 1, 2, 2, 4, 1, 4, 1, 4, 4, 4, 4, 1, 1, 1, 6]
+        "Skill": ["Data Visualization", "Machine Learning", "Data Analysis", "Data Cleaning", "Python", "Java", "R", "SQL/noSQL", "Pandas", "NumPy", "MatPlotLib", "seaborn", "Tensorflow", "Keras", "Torch", "Spark", "PySpark", "Hadoop", "Statistical Analysis"],
+        "Years of Experience":  [4, 1, 2, 2, 4, 1, 4, 1, 4, 4, 4, 4, 1, 1, 1, 1, 2, 1, 6]
     }
     skills_df = pd.DataFrame(skills_data)
 
